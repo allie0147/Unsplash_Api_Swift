72573b040081db85d8a63f74a2838a8fcd875f6d
Allie Kim <allie0147@gmail.com>
2020-11-29 22:24:10 +0900

    Initial Commit

---
+++ Unsplash_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash_API
+//
+//  Created by ishift on 2020/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
